b8db531f3794f512cf9f2e98ba762ed27b7e30f4
KonceptD <kiranjayd@yahoo.com>
2022-07-22 13:27:38 +0200

    HTML: Removed class names from buttons; CSS: - ; JS: Added multiple functions to the functionality of the buttons

--- script.js
+++ script.js
@@ -1,39 +1,12 @@
-// TODO: assign each button to it's numerical function and update display
+// This is making me very frustrated so I'm going to leave this aside and come back with a refreshed mind to tackle it
+// 22/07/2022 - 13:26pm
 
 // git lol "commit msg" - used a git alias to make adding and commiting easier
 
-// Array of operators
-let operators = ['+', '-', '*', '/']
-const displayInput = document.querySelector('#input')
-displayInput.textContent = [' ']
+
+const display = document.querySelector('#input')
 
 
-// function inputScreen (expression) {
-//     const screenOutput = expression; 
-//     return displayInput.textContent = screenOutput;
-// };
-
-function updateDisplay (expression) {
-    return displayInput.textContent = expression;
-};
-
-// const btnZero = document.querySelector('#zero');
-// const btnOne = document.querySelector('#one');
-// const btnTwo = document.querySelector('#two');
-// const btnThree = document.querySelector('#three');
-// const btnFour = document.querySelector('#four');
-// const btnFive = document.querySelector('#five');
-// const btnSix = document.querySelector('#six');
-// const btnSeven = document.querySelector('#seven');
-// const btnEight = document.querySelector('#eight');
-// const btnNine = document.querySelector('#nine');
-// const btnClear = document.querySelector('#clear')
-// const btnMultiply = document.querySelector('#multiply')
-// const btnDivide = document.querySelector('#divide')
-// const btnMinus = document.querySelector('#minus')
-// const btnPlus = document.querySelector('#plus')
-// const btnDot = document.querySelector('#dot')
-// const btnEquals = document.querySelector('#equals')
 
 const keysCon = document.querySelector('.buttons-container')
 
@@ -41,8 +14,19 @@ keysCon.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target
         const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+
+        Array.from(key.paretNode.children)
+            .forEach(i => i.classList.remove('is-depressed'))
 
         if (!action) {
+            if (displayedNum === '0') {
+                displayedNum.textContent = keyContent
+            }
+            else {
+                display.textContent = displayedNum + keyContent
+            }
             console.log('number key')
         }
 
@@ -52,10 +36,12 @@ keysCon.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
             ){
-                console.log('operator key')
+                key.classList.add('is-depressed')
+
              }
-             
+
         if (action === 'decimal') {
+            display.textContent = displayedNum + "."
             console.log('decimal')
         }
         if (action === 'clear') {
@@ -67,36 +53,7 @@ keysCon.addEventListener('click', e => {
         }
 })
 
-// attempting to have a for loop cycle through the buttons container
-// to add each button to an array.
-// const buttonsCon = document.querySelector('buttons-container');
-// let btnNumArray = [document.querySelectorAll('button-number')];
-// btnNumArray.push(document.querySelectorAll('button-number'))
-// let btnOperatorArray = [document.querySelectorAll('button-operator')];
-// let btnFuncArray = [document.querySelectorAll('button-function')];
-// let buttonKey = document.querySelector('button');
 
-// for (let i = 0; i < document.querySelectorAll('.button').length; i++) {
-//     if (i.className == document.querySelector('.button number')) {
-//         btnNumArray.push(i);
-//     }
-//     else if (i.className == document.querySelector('.button operator')) {
-//         btnOperatorArray.push(i)
-//     }
-//     else {
-//         btnFuncArray.push(i);
-//     }
-// }
-
-// for (let i = 0; i < buttonsCon.length(); i++) {
-//     btnArray.push(i);
-
-// }
-// console.log(btnNumArray)
-// console.log(btnOperatorArray)
-// console.log(btnFuncArray)
-
-// inputScreen(2 + 1); need it to display expression and not the sum - currently returning 3
 
 // function to operate and call relevant numerical function according to operator
 function operate (num1, num2, operator) {
