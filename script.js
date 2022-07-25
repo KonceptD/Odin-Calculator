7e3d47aa787c7dcdf6fcb5ded6b4e246c56863ee
KonceptD <kiranjayd@yahoo.com>
2022-07-25 11:50:34 +0200

    HTML:- , CSS: - , JS: Basic aritmetic functionality has been added, equals is working fully, clear button does a complete reset

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // * TODO: Update screen for number display - DONE
-// TODO: Add function for the equals sign to calculate 
+// * TODO: Add function for the equals sign to calculate 
 // TODO: Update the display screen when equals sign is pressed
-// TODO: Add functionality for '+', '-', '/', 'x'
+// * TODO: Add functionality for '+', '-', '/', 'x' - DONE
+// UPDATE: Sort out the equals calc, functionality keeps going to 0
 
 // git lol "commit msg" - used a git alias to make adding and commiting easier
 
@@ -12,6 +13,10 @@
 const display = document.querySelector('#input')
 // assigning the buttons-container to a variable
 const keysContainer = document.querySelector('.buttons-container')
+
+let currentOperator = "empty";; 
+let num1 = [];
+let num2 = [];
 
 // event listener for any key being clicked in the keys container
 keysContainer.addEventListener('click', e => {
@@ -34,70 +39,161 @@ keysContainer.addEventListener('click', e => {
         // if keyPressed doesn't have a dataset then it is a number
         if (!action) {
 
-            // if the number is 0 then send it to the display screen ,else, concat the displayed number and the keyPressed one
+            // if the number is 0 then send it to the display screen ,else , concat the displayed number and the keyPressed one
             if (displayedNum === '0') {
                     displayedNum.textContent = keyContent
-            }
+                    display.textContent = displayedNum + keyContent
+                    if (currentOperator === "empty") {
+                        num1.push(keyContent)
+                    }
+                    if (currentOperator != "empty") {
+                        num2.push(keyContent)
+                    }
+                }
 
             else {
-                display.textContent = displayedNum + keyContent
+                    displayedNum.textContent = keyContent
+                    display.textContent = displayedNum + keyContent
+                    if (currentOperator === "empty") {
+                        num1.push(keyContent)
+                        console.log(num1)
+                    }
+                    if (currentOperator != "empty") {
+                        num2.push(keyContent)
+                    }
                 }
                 
             }
             
             // add a class list to the mathematical signs
-            if (action === 'plus' ||  action === 'minus' || action === 'multiply' || action === 'divide') {
-                key.classList.add('is-depressed')
-                
+        if (action === 'plus' ||  action === 'minus' || action === 'multiply' || action === 'divide') {
+                keyPressed.classList.add('is-depressed')
+
+                if (action === "plus") {
+                    keyPressed.classList.add('is-depressed')
+                    currentOperator = "+"
+                    display.textContent = displayedNum + " " + "+ "
+                    console.log('+') 
+                  }
+
+                else if (action === "minus") {
+                    keyPressed.classList.add('is-depressed')
+                    currentOperator = "-"
+                    display.textContent = displayedNum + " " + "- "
+                    console.log('-') 
                     }
+                
+                else if (action === "multiply") {
+                    keyPressed.classList.add('is-depressed')
+                    currentOperator = "x"
+                    display.textContent = displayedNum + " " + "x "
+                    console.log('x') 
+                    }    
+                
+                else if (action === "divide") {
+                    keyPressed.classList.add('is-depressed')
+                    currentOperator = "/"
+                    display.textContent = displayedNum + " " + "/ "
+                    console.log('/') 
+                    }
+                
+            }
+                
+                    
 
-            // Concat a decimal to the displayed number
-            if (action === 'decimal') {
-                display.textContent = displayedNum + "."
-                console.log('decimal')
-                }
+        // Concat a decimal to the displayed number
+         if (action === 'decimal') {
+            display.textContent = displayedNum + "."
+            console.log('decimal')
+             }
             
-            // empty the value of the display screen when the AC button is hit 
-            if (action === 'clear') {
-                display.textContent = null;
-                console.log('clear')
-                }
+        // empty the value of the display screen when the AC button is hit 
+        if (action === 'clear') {
+            display.textContent = null;
+            currentOperator = "empty"
+            num1 = []
+            num2 = []
+            console.log('clear')
+            }
             
 
-            if (action === 'equals') {
-                 
-                }
-         }   
-    });
+        if (action === 'equals') {
+            if (currentOperator === "+") {
+                let num1AsNum = Number(num1.join(""));
+                let num2AsNum = Number(num2.join(""));
+                console.log(num1AsNum, num2AsNum)
+                display.textContent = addition(num1AsNum, num2AsNum);
+                currentOperator = "empty"
+                num1 = [] 
+                num2 = []
+            }
+            else if (currentOperator === "-") {
+                let num1AsNum = Number(num1.join(""));
+                let num2AsNum = Number(num2.join(""));
+                console.log(num1AsNum, num2AsNum)
+                display.textContent = subtract(num1AsNum, num2AsNum);
+                currentOperator = "empty"
+                num1 = [] 
+                num2 = []
+                
+            }
+            else if (currentOperator === "x") {    
+                let num1AsNum = Number(num1.join(""));
+                let num2AsNum = Number(num2.join(""));
+                console.log(num1AsNum, num2AsNum)
+                display.textContent = multiply(num1AsNum, num2AsNum);
+                currentOperator = "empty"
+                num1 = [] 
+                num2 = []
+                
+            }
+            else if (currentOperator === "/") {
+                let num1AsNum = Number(num1.join(""));
+                let num2AsNum = Number(num2.join(""));
+                console.log(num1AsNum, num2AsNum)
+                display.textContent = divide(num1AsNum, num2AsNum);
+                currentOperator = "empty"
+                num1 = [] 
+                num2 = []
+                
+            }
 
-    function operate (num1, num2, operator) {
-        switch (operator) {
-            case "+":
-                return addition(num1, num2);
-            case "-":
-                return subtract(num1, num2);
-            case "*":
-                return multiply(num1, num2);
-            case "/":
-                return divide(num1, num2);
         }
-    };
+    }   
+});
+
+    // function operate (num1, num2, currentOperator) {
+    //     switch (operatorOperator) {
+    //         case "+":
+    //             return addition(num1, num2);
+    //         case "-":
+    //             return subtract(num1, num2);
+    //         case "*":
+    //             return multiply(num1, num2);
+    //         case "/":
+    //             return divide(num1, num2);
+    //     }
+    // };
     
     // basic functions for the calc: +, -, * and /
     function addition (addOne, addTwo) {
-        return addOne + addTwo;
+        let ans = addOne + addTwo
+        return ans;
     };
     
     function subtract (subOne, subTwo) {
-        return subOne - subTwo;
+        let ans = subOne - subTwo;
+        return ans;
     };
     
     function multiply (mulOne, mulTwo) {
-        return mulOne * mulTwo;
+        let ans = mulOne * mulTwo;
+        return ans;
     };
     
     function divide (divOne, divTwo) {
-        return divOne / divTwo;
+        let ans = divOne / divTwo;
+        return ans;
     };
     
 
